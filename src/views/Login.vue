5afbc0ccc260438ef3068612d06c8f9266788bf8
노누리 <noori105066@gmail.com>
2022-11-24 14:11:04 +0900

    oauth url 수정

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,7 +64,7 @@ export default {
           protocol + "//" + hostName + (port ? ":" + port : "") + "/oauth/google";
       window.location.href =
           `https://accounts.google.com/o/oauth2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=` +
-          "http://localhost:8080/oauth/google"+
+          url+
           "&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
     }
   },
